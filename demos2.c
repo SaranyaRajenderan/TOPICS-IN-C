733fbf3387e3c8672c3a33bc7fe6f63535c97d66
SaranyaRajenderan <saranyathree19@gmail.com>
2025-05-15 09:22:00 +0530

    Add files via upload

---
+++ demos2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+    int main(){
+        char arr[20]="morning";
+        char x[7]={'h','e','l','l','o','\0'};
+        printf("%c",x[4]);
+        printf("\n%s\n",arr);
+        printf("%s",x);
+        return 0;
+    }
+
+    //division
+    #include <stdio.h>
+     void main(){
+        int a=8;
+        int b=3;
+        int c=(a/b);
+        printf();
+     }
